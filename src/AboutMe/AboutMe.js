4b66733714dbfdf362a16210d95457da1f51ec3e
Megan Crotteau <megan.crotteau@gmail.com>
2024-06-24 10:11:55 -0600

    Component architecture

---
+++ src/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import './AboutMe.css';
+import blueSquares from '../assets/blue-squares.png';
+
+function AboutMe() {
+
+    return (
+        <section className="about-me" style={{backgroundImage: `url(${blueSquares})`}}> 
+            <div>hi</div>
+            
+        </section>
+    )
+}
+
+export default AboutMe;
---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AboutMe from '../AboutMe/AboutMe';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+        <AboutMe />
+    </div>
+  );
+}
+
+export default App;
