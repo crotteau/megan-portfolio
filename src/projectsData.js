239f2090b71e0a4bb890d1f2217a13df01b40205
Megan Crotteau <megan.crotteau@gmail.com>
2024-07-11 16:15:19 -0600

    Project data

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,28 +1,37 @@
+import habitat from './assets/habitat.png';
+import asteroid from './assets/asteroid.png';
+import travel from './assets/traveltracker.png';
+import cocktails from './assets/mixmaster1.png';
+
 const projects = [
     {
         name: "Habit-at",
         github: "https://github.com/Habit-at-2311/fe-habit-at",
         deployed:"https://habit-at-2.vercel.app/",
-        description: "Visualize tracked habits through 3D flower models."
+        description: "Visualize tracked habits through 3D flower models.",
+        image: habitat
     },
     {
         name: "Asteroid Patrol",
         github: "https://github.com/crotteau/asteroids",
         deployed:"https://asteroids-blond.vercel.app/",
-        description: "Daily updates of nearby asteroids!"
+        description: "Daily updates of nearby asteroids!",
+        image: asteroid
     },
     {
         name: "Travel Tracker",
         github: "https://github.com/crotteau/travelTracker",
         deployed:"https://crotteau.github.io/travelTracker/",
-        description: "Track upcoming and past vacations."
+        description: "Track upcoming and past vacations.",
+        image: travel
     },
     {
         name: "Mix Master",
         github: "https://github.com/crotteau/mix-master-fe",
         deployed: null,
-        description: "A source for finding mocktail and cocktail recipes."
+        description: "A source for finding mocktail and cocktail recipes.",
+        image: cocktails
     }
 ];
 
-module.exports = projects
+export default projects;
