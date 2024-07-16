5c740335266f5b6b8265ac9adcc8122460a6a745
Megan Crotteau <megan.crotteau@gmail.com>
2024-07-16 10:27:19 -0600

    Project links

--- src/ProjectsGrid/ProjectsGrid.js
+++ src/ProjectsGrid/ProjectsGrid.js
@@ -1,8 +1,10 @@
 import './ProjectsGrid.css';
 import grid from '../assets/green-squares.png';
-import habitat from '../assets/habitat.png';
 import projects from '../projectsData';
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 function ProjectsGrid() {
    const [myProjects, setProjects] = useState([])
@@ -14,20 +16,39 @@ function ProjectsGrid() {
    const showProjects = () => {
       const projectInfo = projects.map(project => {
          return (
-            <article>
+            <article key={project.name}>
                <p>{project.name}</p>
                <p>{project.description}</p>
-               <img src={project.image} alt='website-homepage' className='project-image'/>
+               <img src={project.image} alt='website-homepage' className='project-image' />
+               <div>
+                  <a href={project.github}>
+                     <FontAwesomeIcon
+                        className='project-icon'
+                        icon={faGithub}
+                        style={{ fontSize: '30px', padding: '5px' }}
+                     />
+                  </a>
+                  {project.deployed &&
+                     <a href={project.deployed}>
+                        <FontAwesomeIcon
+                           className='project-icon'
+                           icon={faLink}
+                           style={{ fontSize: '30px', padding: '5px' }}
+                        />
+                     </a>
+                  }
+               </div>
             </article>
          )
       })
       setProjects(projectInfo)
    }
-   console.log(myProjects)
+
    return (
       //  <section className='projects-grid' style={{ backgroundImage: `url(${grid})` }}> 
       <section className='projects-grid'>
          {myProjects}
+         {/* <img src={squiggle}/> */}
       </section>
    )
 }
